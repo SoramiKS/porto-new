8c33271052c832a49fbb1930ba1dfcde390a58d9
SoramiKS <freitas.emiliano1@gmail.com>
2025-01-08 14:08:27 +0700

    Update Skills.jsx

--- Template Portofolio/src/pages/home/components/Skills.jsx
+++ Template Portofolio/src/pages/home/components/Skills.jsx
@@ -9,7 +9,7 @@ const Skills = () => {
     return (
         <section className="bg-blue-100 py-16 px-8 md:py-24 md:px-16 items-center object-center">
             <h2 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-8 text-center text-slate-800">
-                Skills
+                Projecet
             </h2>
             <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-1 px-0 md:px-48 ">
             <CardWithRippleExample />
