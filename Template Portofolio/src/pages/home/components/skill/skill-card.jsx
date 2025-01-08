0004abd513fced6986983a22fe2020ae45f0f062
SoramiKS <freitas.emiliano1@gmail.com>
2025-01-08 14:15:39 +0700

    Update skill-card.jsx

--- Template Portofolio/src/pages/home/components/skill/skill-card.jsx
+++ Template Portofolio/src/pages/home/components/skill/skill-card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { TERipple } from 'tw-elements-react';
 
-export default function CardWithRippleExample({ image, title, content }) {
+export default function CardWithRippleExample({ image, title, content, linknya }) {
   return (
     <div className="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
       <TERipple>
