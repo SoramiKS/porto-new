a04567d61f374f6a5f50129091f751847b25f7e5
Emiliano S.F. <freitas.emiliano1@gmail.com>
2025-01-16 22:43:26 +0700

    Delete Template Portofolio directory

--- Template Portofolio/src/components/Container.jsx
+++
@@ -1,9 +0,0 @@
-export default function Container({ children }) {
-    return (
-        <section className='bg-gray-100 w-full flex justify-center'>
-            <div className="flex flex-col gap-y-3 bg-blue-100">
-                {children}
-            </div>
-        </section>
-    )
-}
