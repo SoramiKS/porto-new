e84e50d8bbc2659d3b69f1505feddeaae4423374
Emiliano S.F. <freitas.emiliano1@gmail.com>
2025-01-21 00:15:25 +0700

    Delete Template Portofolio directory

--- Template Portofolio/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-slate-800 text-white text-center p-4">
-            <p>&copy; 2024 - JarPortof.</p>
-        </footer>
-    );
-}
